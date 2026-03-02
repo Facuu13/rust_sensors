f29c0d91c6a8875ebc9492e6c4773e0053af50c9
Facuu13 <facundoo30@gmail.com>
2026-03-02 10:57:20 -0300

    vector

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 struct Sensor {
     id: String,
     temperature: f32,
+    history: Vec<f32>,
 }
 
 impl Sensor {
@@ -8,11 +9,13 @@ impl Sensor {
         Self {
             id: id.to_string(),
             temperature,
+            history: vec![temperature], // arranca con el primer valor
         }
     }
 
     fn update_temperature(&mut self, new_temp: f32) {
         self.temperature = new_temp;
+        self.history.push(new_temp);
     }
 
     fn print(&self) {
@@ -22,16 +25,21 @@ impl Sensor {
     fn is_hot(&self) -> bool {
         self.temperature > 28.0
     }
+
+    fn print_history(&self) {
+        println!("History for {}: {:?}", self.id, self.history);
+    }
 }
 
 fn main() {
     let mut sensor = Sensor::new("esp32-01", 25.0);
 
-    sensor.print();
+    sensor.update_temperature(27.0);
+    sensor.update_temperature(29.5);
+    sensor.update_temperature(30.1);
 
-    sensor.update_temperature(31.5);
-
     sensor.print();
+    sensor.print_history();
 
     if sensor.is_hot() {
         println!("🔥 Sensor is hot!");
